ac26a671386428cf6463f76399ecfaaa4547262b
Adam Franco <afranco@middlebury.edu>
2012-11-28 21:33:57 -0500

    Moved output writing to new output module

--- curvature.py
+++ curvature.py
@@ -33,16 +33,20 @@
 # Copyright 2012 Adam Franco
 # License: GNU General Public License Version 3 or later
 
-import codecs
+import os
 import argparse
 from curvature.collector import WayCollector
 from curvature.filter import WayFilter
+from curvature.output import TabOutput
+from curvature.output import SingleColorKmlOutput
+from curvature.output import MultiColorKmlOutput
 
 parser = argparse.ArgumentParser(description='Find the roads that are most twisty in an Open Street Map (OSM) XML file.')
 parser.add_argument('-v', action='store_true', help='Verbose mode, showing status output')
-parser.add_argument('-q', action='store_true', help='Quiet mode, do not output tabular data')
+parser.add_argument('-t', action='store_true', help='Display tabular output')
 parser.add_argument('--no_kml', action='store_true', help='Do not generate a KML file. By default a KML file is generated with the name of the input file followed by .kml')
-parser.add_argument('--kml_file', type=argparse.FileType('w'), help='A custom destination for the KML file')
+parser.add_argument('--output_path', type=str, help='The path under which output files should be written')
+parser.add_argument('--output_basename', type=str, help='The base of the name for output files. This will be appended with a suffix and extension')
 parser.add_argument('--kml_colorize', action='store_true', help='Colorize KML lines based on the curvature of the road at each segment. Without this option roads will be lines of a single color. For large regions this may make Google Earth run slowly.')
 parser.add_argument('--min_length', type=float, help='the minimum length of a way that should be included, in miles, 0 for no minimum. The default is 2.0')
 parser.add_argument('--max_length', type=float, help='the maximum length of a way that should be included, in miles, 0 for no maximum. The default is 0')
@@ -114,119 +118,28 @@ if args.max_lon_bound is not None:
 # start parsing
 collector.load_file(args.file.name)
 
-# Filter out ways that are too short/long or too straight or too curvy
-ways = default_filter.filter(collector.ways)
-
-# Sort the ways based on curvature
-ways = sorted(ways, key=lambda k: k['curvature'])
 
 # Output our tabular data
-if not args.q:
-	print "Curvature	Length (mi) Distance (mi)	Id				Name  			County"
-	for way in ways:
-		print '%d	%9.2f	%9.2f	%10s	%25s	%20s' % (way['curvature'], way['length'] / 1609, way['distance'] / 1609, way['id'], way['name'], way['county'])
+if args.t:
+	tab = TabOutput(default_filter)
+	tab.output(collector.ways)
 
-def write_way_kml_colorize(f, way):
-	f.write('	<Folder>\n')
-	f.write('		<styleUrl>#folderStyle</styleUrl>\n')
-	f.write('		<name>' + way['name'] + '</name>\n')
-	f.write('		<description>' + 'Curvature: %.2f\nDistance: %.2f mi\nType: %s\nSurface: %s' % (way['curvature'], way['length'] / 1609, way['type'], way['surface']) + '</description>\n')
-	current_curvature_level = 0
-	i = 0
-	for segment in way['segments']:
-		if segment['curvature_level'] != current_curvature_level or not i:
-			current_curvature_level = segment['curvature_level']
-			# Close the open LineString
-			if i:
-				f.write('</coordinates>\n')
-				f.write('			</LineString>\n')
-				f.write('		</Placemark>\n')
-			# Start a new linestring for this level
-			f.write('		<Placemark>\n')
-			f.write('			<styleUrl>#lineStyle%d</styleUrl>\n' % (current_curvature_level))
-			f.write('			<LineString>\n')
-			f.write('				<tessellate>1</tessellate>\n')
-			f.write('				<coordinates>')
-			f.write("%.6f,%6f " %(segment['start']['lon'], segment['start']['lat']))
-		f.write("%.6f,%6f " %(segment['end']['lon'], segment['end']['lat']))
-		i = i + 1
-	if i:
-		f.write('</coordinates>\n')
-		f.write('			</LineString>\n')
-		f.write('		</Placemark>\n')
-	f.write('	</Folder>\n')
-	
-def write_way_kml_single_color(f, way):
-	f.write('	<Placemark>\n')
-	f.write('		<styleUrl>#lineStyle4</styleUrl>\n')
-	f.write('		<name>' + way['name'] + '</name>\n')
-	f.write('		<description>' + 'Curvature: %.2f\nDistance: %.2f mi\nType: %s\nSurface: %s' % (way['curvature'], way['length'] / 1609, way['type'], way['surface']) + '</description>\n')
-	f.write('		<LineString>\n')
-	f.write('			<tessellate>1</tessellate>\n')
-	f.write('			<coordinates>')
-	f.write("%.6f,%6f " %(way['segments'][0]['start']['lon'], way['segments'][0]['start']['lat']))
-	for segment in way['segments']:
-		f.write("%.6f,%6f " %(segment['end']['lon'], segment['end']['lat']))
-
-	f.write('</coordinates>\n')
-	f.write('		</LineString>\n')
-	f.write('	</Placemark>\n')
-
+# Generate KML output
 if not args.no_kml:
-	if args.kml_file is None:
-		kml_file = args.file.name + '.kml'
+	if args.output_path is None:
+		path = os.path.dirname(args.file.name)
 	else:
-		kml_file = args.kml_file.name
-	# Generate KML output
-	f = codecs.open(kml_file, 'w', "utf-8")
-	f.write('<?xml version="1.0" encoding="UTF-8"?>\n')
-	f.write('<kml xmlns="http://www.opengis.net/kml/2.2" xmlns:gx="http://www.google.com/kml/ext/2.2" xmlns:kml="http://www.opengis.net/kml/2.2" xmlns:atom="http://www.w3.org/2005/Atom">\n')
-	f.write('<Document>\n')
-	# Straight roads
-	f.write('	<Style id="lineStyle0">\n')
-	f.write('		<LineStyle>\n')
-	f.write('			<color>F000E010</color>\n')
-	f.write('			<width>4</width>\n')
-	f.write('		</LineStyle>\n')
-	f.write('	</Style>\n')
-	# Level 1 turns
-	f.write('	<Style id="lineStyle1">\n')
-	f.write('		<LineStyle>\n')
-	f.write('			<color>F000FFFF</color>\n')
-	f.write('			<width>4</width>\n')
-	f.write('		</LineStyle>\n')
-	f.write('	</Style>\n')
-	# Level 2 turns
-	f.write('	<Style id="lineStyle2">\n')
-	f.write('		<LineStyle>\n')
-	f.write('			<color>F000AAFF</color>\n')
-	f.write('			<width>4</width>\n')
-	f.write('		</LineStyle>\n')
-	f.write('	</Style>\n')
-	# Level 3 turns
-	f.write('	<Style id="lineStyle3">\n')
-	f.write('		<LineStyle>\n')
-	f.write('			<color>F00055FF</color>\n')
-	f.write('			<width>4</width>\n')
-	f.write('		</LineStyle>\n')
-	f.write('	</Style>\n')
-	# Level 4 turns
-	f.write('	<Style id="lineStyle4">\n')
-	f.write('		<LineStyle>\n')
-	f.write('			<color>F00000FF</color>\n')
-	f.write('			<width>4</width>\n')
-	f.write('		</LineStyle>\n')
-	f.write('	</Style>\n')
-	f.write('	<Style id="folderStyle">\n')
-	f.write('		<ListStyle>\n')
-	f.write('			<listItemType>checkHideChildren</listItemType>\n')
-	f.write('		</ListStyle>\n')
-	f.write('	</Style>\n')
-	ways.reverse()
-	for way in ways:
-		if args.kml_colorize:
-			write_way_kml_colorize(f, way)
-		else:
-			write_way_kml_single_color(f, way)
-	f.write('</Document>\n')
-	f.write('</kml>\n')
+		path = args.output_path
+	if args.output_basename is None:
+		basename = os.path.basename(args.file.name)
+		parts = os.path.splitext(basename)
+		basename = parts[0]
+	else:
+		basename = os.path.basename(args.output_basename)
+		
+	if args.kml_colorize:
+		kml = MultiColorKmlOutput(default_filter)
+	else:
+		kml = SingleColorKmlOutput(default_filter)
+	kml.write(collector.ways, path, basename)
+	
---
+++ curvature/output.py
@@ -0,0 +1,126 @@
+class Output(object):
+	def __init__(self, filter):
+		self.filter = filter
+	
+	def filter_and_sort(self, ways):
+		# Filter out ways that are too short/long or too straight or too curvy
+		ways = self.filter.filter(ways)
+		
+		# Sort the ways based on curvature
+		ways = sorted(ways, key=lambda k: k['curvature'])
+		
+		return ways
+		
+class TabOutput(Output):
+	def output(self, ways):
+		ways = self.filter_and_sort(ways)
+		
+		print "Curvature	Length (mi) Distance (mi)	Id				Name  			County"
+		for way in ways:
+			print '%d	%9.2f	%9.2f	%10s	%25s	%20s' % (way['curvature'], way['length'] / 1609, way['distance'] / 1609, way['id'], way['name'], way['county'])
+
+import codecs
+class KmlOutput(Output):
+	def _write_header(self, f):
+		f.write('<?xml version="1.0" encoding="UTF-8"?>\n')
+		f.write('<kml xmlns="http://www.opengis.net/kml/2.2" xmlns:gx="http://www.google.com/kml/ext/2.2" xmlns:kml="http://www.opengis.net/kml/2.2" xmlns:atom="http://www.w3.org/2005/Atom">\n')
+		f.write('<Document>\n')
+		# Straight roads
+		f.write('	<Style id="lineStyle0">\n')
+		f.write('		<LineStyle>\n')
+		f.write('			<color>F000E010</color>\n')
+		f.write('			<width>4</width>\n')
+		f.write('		</LineStyle>\n')
+		f.write('	</Style>\n')
+		# Level 1 turns
+		f.write('	<Style id="lineStyle1">\n')
+		f.write('		<LineStyle>\n')
+		f.write('			<color>F000FFFF</color>\n')
+		f.write('			<width>4</width>\n')
+		f.write('		</LineStyle>\n')
+		f.write('	</Style>\n')
+		# Level 2 turns
+		f.write('	<Style id="lineStyle2">\n')
+		f.write('		<LineStyle>\n')
+		f.write('			<color>F000AAFF</color>\n')
+		f.write('			<width>4</width>\n')
+		f.write('		</LineStyle>\n')
+		f.write('	</Style>\n')
+		# Level 3 turns
+		f.write('	<Style id="lineStyle3">\n')
+		f.write('		<LineStyle>\n')
+		f.write('			<color>F00055FF</color>\n')
+		f.write('			<width>4</width>\n')
+		f.write('		</LineStyle>\n')
+		f.write('	</Style>\n')
+		# Level 4 turns
+		f.write('	<Style id="lineStyle4">\n')
+		f.write('		<LineStyle>\n')
+		f.write('			<color>F00000FF</color>\n')
+		f.write('			<width>4</width>\n')
+		f.write('		</LineStyle>\n')
+		f.write('	</Style>\n')
+		f.write('	<Style id="folderStyle">\n')
+		f.write('		<ListStyle>\n')
+		f.write('			<listItemType>checkHideChildren</listItemType>\n')
+		f.write('		</ListStyle>\n')
+		f.write('	</Style>\n')
+	
+	def _write_footer(self, f):
+		f.write('</Document>\n')
+		f.write('</kml>\n')
+	
+	def _filename_suffix(self):
+		return ''
+		
+	def write (self, ways, path, basename):
+		ways = self.filter_and_sort(ways)
+		ways.reverse()
+		
+		filename = basename + '-{0:.0f}'.format(self.filter.min_curvature)
+		if self.filter.max_curvature > 0:
+			filename += '-{0:.0f}'.format(self.filter.max_curvature)
+		filename += self._filename_suffix() + '.kml'
+		f = codecs.open(path + '/' + filename, 'w', "utf-8")
+		
+		self._write_header(f)
+		self._write_ways(f, ways)
+		self._write_footer(f)
+
+class SingleColorKmlOutput(KmlOutput):
+	
+	def _write_ways(self, f, ways):
+		for way in ways:
+			f.write('	<Placemark>\n')
+			f.write('		<styleUrl>#lineStyle4</styleUrl>\n')
+			f.write('		<name>' + way['name'] + '</name>\n')
+			f.write('		<description>' + 'Curvature: %.2f\nDistance: %.2f mi\nType: %s\nSurface: %s' % (way['curvature'], way['length'] / 1609, way['type'], way['surface']) + '</description>\n')
+			f.write('		<LineString>\n')
+			f.write('			<tessellate>1</tessellate>\n')
+			f.write('			<coordinates>')
+			f.write("%.6f,%6f " %(way['segments'][0]['start']['lon'], way['segments'][0]['start']['lat']))
+			for segment in way['segments']:
+				f.write("%.6f,%6f " %(segment['end']['lon'], segment['end']['lat']))
+			f.write('</coordinates>\n')
+			f.write('		</LineString>\n')
+			f.write('	</Placemark>\n')
+
+class MultiColorKmlOutput(KmlOutput):
+	def _filename_suffix(self):
+		return '.multicolor'
+	
+	def _write_ways(self, f, ways):
+		for way in ways:
+			f.write('	<Placemark>\n')
+			f.write('		<styleUrl>#lineStyle4</styleUrl>\n')
+			f.write('		<name>' + way['name'] + '</name>\n')
+			f.write('		<description>' + 'Curvature: %.2f\nDistance: %.2f mi\nType: %s\nSurface: %s' % (way['curvature'], way['length'] / 1609, way['type'], way['surface']) + '</description>\n')
+			f.write('		<LineString>\n')
+			f.write('			<tessellate>1</tessellate>\n')
+			f.write('			<coordinates>')
+			f.write("%.6f,%6f " %(way['segments'][0]['start']['lon'], way['segments'][0]['start']['lat']))
+			for segment in way['segments']:
+				f.write("%.6f,%6f " %(segment['end']['lon'], segment['end']['lat']))
+			f.write('</coordinates>\n')
+			f.write('		</LineString>\n')
+			f.write('	</Placemark>\n')
