0b28cedaa9d95d41d46cedfafa9398d54796533c
Adam Franco <afranco@middlebury.edu>
2012-11-30 00:19:21 -0500

    Now using default values on arguments to simplify the script.

--- curvature.py
+++ curvature.py
@@ -48,28 +48,28 @@ parser = argparse.ArgumentParser(description='Find the roads that are most twist
 parser.add_argument('-v', action='store_true', help='Verbose mode, showing status output')
 parser.add_argument('-t', action='store_true', help='Display tabular output')
 parser.add_argument('--no_kml', action='store_true', help='Do not generate a KML file. By default a KML file is generated with the name of the input file followed by .kml')
-parser.add_argument('--output_path', type=str, help='The path under which output files should be written')
-parser.add_argument('--output_basename', type=str, help='The base of the name for output files. This will be appended with a suffix and extension')
+parser.add_argument('--output_path', type=str, default='.', help='The path under which output files should be written')
+parser.add_argument('--output_basename', type=str, default=None, help='The base of the name for output files. This will be appended with a suffix and extension')
 parser.add_argument('--colorize', action='store_true', help='Colorize KML lines based on the curvature of the road at each segment. Without this option roads will be lines of a single color. For large regions this may make Google Earth run slowly.')
-parser.add_argument('--min_length', type=float, help='the minimum length of a way that should be included, in miles, 0 for no minimum. The default is 2.0')
-parser.add_argument('--max_length', type=float, help='the maximum length of a way that should be included, in miles, 0 for no maximum. The default is 0')
-parser.add_argument('--min_curvature', type=float, help='the minimum curvature of a way that should be included, 0 for no minimum. The default is 300 which catches most twisty roads.')
-parser.add_argument('--max_curvature', type=float, help='the maximum curvature of a way that should be included, 0 for no maximum. The default is 0')
+parser.add_argument('--min_length', type=float, default=1, help='the minimum length of a way that should be included, in miles, 0 for no minimum. The default is 2.0')
+parser.add_argument('--max_length', type=float, default=0, help='the maximum length of a way that should be included, in miles, 0 for no maximum. The default is 0')
+parser.add_argument('--min_curvature', type=float, default=300, help='the minimum curvature of a way that should be included, 0 for no minimum. The default is 300 which catches most twisty roads.')
+parser.add_argument('--max_curvature', type=float, default=0, help='the maximum curvature of a way that should be included, 0 for no maximum. The default is 0')
 parser.add_argument('--add_kml', metavar='PARAMETERS', type=str, action='append', help='Output an additional KML file with alternate output parameters. PARAMETERS should be a comma-separated list of option=value that may include any of the following options: colorize, min_curvature, max_curvature, min_length, and max_length. Example: --add_kml colorize=1,min_curvature=1000')
-parser.add_argument('--level_1_max_radius', type=int, help='the maximum radius of a curve (in meters) that will be considered part of level 1. Curves with radii larger than this will be considered straight. The default is 175')
-parser.add_argument('--level_1_weight', type=float, help='the weight to give segments that are classified as level 1. Default 1')
-parser.add_argument('--level_2_max_radius', type=int, help='the maximum radius of a curve (in meters) that will be considered part of level 2. The default is 100')
-parser.add_argument('--level_2_weight', type=float, help='the weight to give segments that are classified as level 2. Default 1.3')
-parser.add_argument('--level_3_max_radius', type=int, help='the maximum radius of a curve (in meters) that will be considered part of level 3. The default is 60')
-parser.add_argument('--level_3_weight', type=float, help='the weight to give segments that are classified as level 3. Default 1.6')
-parser.add_argument('--level_4_max_radius', type=int, help='the maximum radius of a curve (in meters) that will be considered part of level 4. The default is 30')
-parser.add_argument('--level_4_weight', type=float, help='the weight to give segments that are classified as level 4. Default 2')
-parser.add_argument('--ignored_surfaces', help='a list of the surfaces that should be ignored. The default is dirt,unpaved,gravel,sand,grass,ground')
-parser.add_argument('--highway_types', help='a list of the highway types that should be included. The default is secondary,residential,tertiary,primary,primary_link,motorway,motorway_link,road,trunk,trunk_link,unclassified')
-parser.add_argument('--min_lat_bound', type=float, help='The minimum latitude to include.')
-parser.add_argument('--max_lat_bound', type=float, help='The maximum latitude to include.')
-parser.add_argument('--min_lon_bound', type=float, help='The minimum longitude to include.')
-parser.add_argument('--max_lon_bound', type=float, help='The maximum longitude to include.')
+parser.add_argument('--level_1_max_radius', type=int, default=175, help='the maximum radius of a curve (in meters) that will be considered part of level 1. Curves with radii larger than this will be considered straight. The default is 175')
+parser.add_argument('--level_1_weight', type=float, default=1, help='the weight to give segments that are classified as level 1. Default 1')
+parser.add_argument('--level_2_max_radius', type=int, default=100, help='the maximum radius of a curve (in meters) that will be considered part of level 2. The default is 100')
+parser.add_argument('--level_2_weight', type=float, default=1.3, help='the weight to give segments that are classified as level 2. Default 1.3')
+parser.add_argument('--level_3_max_radius', type=int, default=60, help='the maximum radius of a curve (in meters) that will be considered part of level 3. The default is 60')
+parser.add_argument('--level_3_weight', type=float, default=1.6, help='the weight to give segments that are classified as level 3. Default 1.6')
+parser.add_argument('--level_4_max_radius', type=int, default=30, help='the maximum radius of a curve (in meters) that will be considered part of level 4. The default is 30')
+parser.add_argument('--level_4_weight', type=float, default=2, help='the weight to give segments that are classified as level 4. Default 2')
+parser.add_argument('--ignored_surfaces', type=str, default='dirt,unpaved,gravel,sand,grass,ground', help='a list of the surfaces that should be ignored. The default is dirt,unpaved,gravel,sand,grass,ground')
+parser.add_argument('--highway_types', type=str, default='secondary,residential,tertiary,primary,primary_link,motorway,motorway_link,road,trunk,trunk_link,unclassified', help='a list of the highway types that should be included. The default is secondary,residential,tertiary,primary,primary_link,motorway,motorway_link,road,trunk,trunk_link,unclassified')
+parser.add_argument('--min_lat_bound', type=float, default=None, help='The minimum latitude to include.')
+parser.add_argument('--max_lat_bound', type=float, default=None, help='The maximum latitude to include.')
+parser.add_argument('--min_lon_bound', type=float, default=None, help='The minimum longitude to include.')
+parser.add_argument('--max_lon_bound', type=float, default=None, help='The maximum longitude to include.')
 parser.add_argument('file', type=argparse.FileType('r'), nargs='+', help='the input file. Should be an OSM XML file.')
 args = parser.parse_args()
 
@@ -82,42 +82,24 @@ default_filter = WayFilter()
 
 # Configure settings based on the command-line arguments
 collector.verbose = args.v
-if args.min_length is not None:
-	default_filter.min_length = args.min_length
-if args.max_length is not None:
-	default_filter.max_length = args.max_length
-if args.min_curvature is not None:
-	default_filter.min_curvature = args.min_curvature
-if args.max_curvature is not None:
-	default_filter.max_curvature = args.max_curvature
-if args.ignored_surfaces is not None:
-	collector.ignored_surfaces = args.ignored_surfaces.split(',')
-if args.highway_types is not None:
-	collector.roads = args.highway_types.split(',')
-if args.level_1_max_radius is not None:
-	collector.level_1_max_radius = args.level_1_max_radius
-if args.level_1_weight is not None:
-	collector.level_1_weight = args.level_1_weight
-if args.level_2_max_radius is not None:
-	collector.level_2_max_radius = args.level_2_max_radius
-if args.level_2_weight is not None:
-	collector.level_2_weight = args.level_2_weight
-if args.level_3_max_radius is not None:
-	collector.level_3_max_radius = args.level_3_max_radius
-if args.level_3_weight is not None:
-	collector.level_3_weight = args.level_3_weight
-if args.level_4_max_radius is not None:
-	collector.level_4_max_radius = args.level_4_max_radius
-if args.level_4_weight is not None:
-	collector.level_4_weight = args.level_4_weight
-if args.min_lat_bound is not None:
-	collector.min_lat_bound = args.min_lat_bound
-if args.max_lat_bound is not None:
-	collector.max_lat_bound = args.max_lat_bound
-if args.min_lon_bound is not None:
-	collector.min_lon_bound = args.min_lon_bound
-if args.max_lon_bound is not None:
-	collector.max_lon_bound = args.max_lon_bound
+default_filter.min_length = args.min_length
+default_filter.max_length = args.max_length
+default_filter.min_curvature = args.min_curvature
+default_filter.max_curvature = args.max_curvature
+collector.ignored_surfaces = args.ignored_surfaces.split(',')
+collector.roads = args.highway_types.split(',')
+collector.level_1_max_radius = args.level_1_max_radius
+collector.level_1_weight = args.level_1_weight
+collector.level_2_max_radius = args.level_2_max_radius
+collector.level_2_weight = args.level_2_weight
+collector.level_3_max_radius = args.level_3_max_radius
+collector.level_3_weight = args.level_3_weight
+collector.level_4_max_radius = args.level_4_max_radius
+collector.level_4_weight = args.level_4_weight
+collector.min_lat_bound = args.min_lat_bound
+collector.max_lat_bound = args.max_lat_bound
+collector.min_lon_bound = args.min_lon_bound
+collector.max_lon_bound = args.max_lon_bound
 
 # start parsing
 for file in args.file:
