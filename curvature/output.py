0a1eb32a238004fbd4dfac673dc42318dec1e1c4
Adam Franco <afranco@middlebury.edu>
2012-12-06 10:04:48 -0500

    Refactored output classes to be more extensable and include less hard-coded XML

--- curvature/output.py
+++ curvature/output.py
@@ -1,3 +1,5 @@
+import sys
+
 class Output(object):
 	def __init__(self, filter):
 		self.filter = filter
@@ -23,49 +25,38 @@ import codecs
 from xml.sax.saxutils import escape
 class KmlOutput(Output):
 	def _write_header(self, f):
+		self._write_doc_start(f)
+		self._write_styles(f, self.get_styles())
+	
+	def _write_doc_start(self, f):
 		f.write('<?xml version="1.0" encoding="UTF-8"?>\n')
 		f.write('<kml xmlns="http://www.opengis.net/kml/2.2" xmlns:gx="http://www.google.com/kml/ext/2.2" xmlns:kml="http://www.opengis.net/kml/2.2" xmlns:atom="http://www.w3.org/2005/Atom">\n')
 		f.write('<Document>\n')
-		# Straight roads
-		f.write('	<Style id="lineStyle0">\n')
-		f.write('		<LineStyle>\n')
-		f.write('			<color>F000E010</color>\n')
-		f.write('			<width>4</width>\n')
-		f.write('		</LineStyle>\n')
-		f.write('	</Style>\n')
-		# Level 1 turns
-		f.write('	<Style id="lineStyle1">\n')
-		f.write('		<LineStyle>\n')
-		f.write('			<color>F000FFFF</color>\n')
-		f.write('			<width>4</width>\n')
-		f.write('		</LineStyle>\n')
-		f.write('	</Style>\n')
-		# Level 2 turns
-		f.write('	<Style id="lineStyle2">\n')
-		f.write('		<LineStyle>\n')
-		f.write('			<color>F000AAFF</color>\n')
-		f.write('			<width>4</width>\n')
-		f.write('		</LineStyle>\n')
-		f.write('	</Style>\n')
-		# Level 3 turns
-		f.write('	<Style id="lineStyle3">\n')
-		f.write('		<LineStyle>\n')
-		f.write('			<color>F00055FF</color>\n')
-		f.write('			<width>4</width>\n')
-		f.write('		</LineStyle>\n')
-		f.write('	</Style>\n')
-		# Level 4 turns
-		f.write('	<Style id="lineStyle4">\n')
-		f.write('		<LineStyle>\n')
-		f.write('			<color>F00000FF</color>\n')
-		f.write('			<width>4</width>\n')
-		f.write('		</LineStyle>\n')
-		f.write('	</Style>\n')
-		f.write('	<Style id="folderStyle">\n')
-		f.write('		<ListStyle>\n')
-		f.write('			<listItemType>checkHideChildren</listItemType>\n')
-		f.write('		</ListStyle>\n')
-		f.write('	</Style>\n')
+	
+	def get_styles(self):
+		return {
+			'lineStyle0':{'color':'F000E010'}, # Straight roads
+			'lineStyle1':{'color':'F000FFFF'}, # Level 1 turns
+			'lineStyle2':{'color':'F000AAFF'}, # Level 2 turns
+			'lineStyle3':{'color':'F00055FF'}, # Level 3 turns
+			'lineStyle4':{'color':'F00000FF'}, # Level 4 turns
+		}
+			
+		
+	def _write_styles(self, f, styles):
+		for id in styles:
+			style = styles[id]
+			if 'width' not in style:
+				style['width'] = '4'
+			if 'color' not in style:
+				style['color'] = 'F0FFFFFF'
+			
+			f.write('	<Style id="' + id + '">\n')
+			f.write('		<LineStyle>\n')
+			f.write('			<color>' + style['color'] + '</color>\n')
+			f.write('			<width>' + style['width'] + '</width>\n')
+			f.write('		</LineStyle>\n')
+			f.write('	</Style>\n')		
 	
 	def _write_footer(self, f):
 		f.write('</Document>\n')
@@ -78,6 +69,14 @@ class KmlOutput(Output):
 		ways = self.filter_and_sort(ways)
 		ways.reverse()
 		
+		f = codecs.open(path + '/' + self.get_filename(basename), 'w', "utf-8")
+		
+		self._write_header(f)
+		self._write_ways(f, ways)
+		self._write_footer(f)
+		f.close()
+	
+	def get_filename(self, basename):
 		filename = basename + '.c_{0:.0f}'.format(self.filter.min_curvature)
 		if self.filter.max_curvature > 0:
 			filename += '-{0:.0f}'.format(self.filter.max_curvature)
@@ -86,21 +85,22 @@ class KmlOutput(Output):
 		if self.filter.max_length > 0:
 			filename += '-{0:.0f}'.format(self.filter.max_length)
 		filename += self._filename_suffix() + '.kml'
-		f = codecs.open(path + '/' + filename, 'w', "utf-8")
-		
-		self._write_header(f)
-		self._write_ways(f, ways)
-		self._write_footer(f)
-		f.close()
+		return filename;
+	
+	def get_description(self, way):
+		return 'Curvature: %.2f\nDistance: %.2f mi\nType: %s\nSurface: %s' % (way['curvature'], way['length'] / 1609, way['type'], way['surface']) 
 
 class SingleColorKmlOutput(KmlOutput):
 	
 	def _write_ways(self, f, ways):
 		for way in ways:
+			if 'segments' not in way or not len(way['segments']):
+# 				sys.stderr.write("\nError: way has no segments: {} \n".format(way['name']))
+				continue
 			f.write('	<Placemark>\n')
-			f.write('		<styleUrl>#lineStyle4</styleUrl>\n')
+			f.write('		<styleUrl>#' + self.line_style(way) + '</styleUrl>\n')
 			f.write('		<name>' + escape(way['name']) + '</name>\n')
-			f.write('		<description>' + 'Curvature: %.2f\nDistance: %.2f mi\nType: %s\nSurface: %s' % (way['curvature'], way['length'] / 1609, way['type'], way['surface']) + '</description>\n')
+			f.write('		<description>' + self.get_description(way) + '</description>\n')
 			f.write('		<LineString>\n')
 			f.write('			<tessellate>1</tessellate>\n')
 			f.write('			<coordinates>')
@@ -110,17 +110,26 @@ class SingleColorKmlOutput(KmlOutput):
 			f.write('</coordinates>\n')
 			f.write('		</LineString>\n')
 			f.write('	</Placemark>\n')
+	
+	def line_style(self, way):
+		return 'lineStyle4'
 
 class MultiColorKmlOutput(KmlOutput):
 	def _filename_suffix(self):
 		return '.multicolor'
 	
 	def _write_ways(self, f, ways):
+		f.write('	<Style id="folderStyle">\n')
+		f.write('		<ListStyle>\n')
+		f.write('			<listItemType>checkHideChildren</listItemType>\n')
+		f.write('		</ListStyle>\n')
+		f.write('	</Style>\n')
+		
 		for way in ways:
 			f.write('	<Folder>\n')
 			f.write('		<styleUrl>#folderStyle</styleUrl>\n')
 			f.write('		<name>' + escape(way['name']) + '</name>\n')
-			f.write('		<description>' + 'Curvature: %.2f\nDistance: %.2f mi\nType: %s\nSurface: %s' % (way['curvature'], way['length'] / 1609, way['type'], way['surface']) + '</description>\n')
+			f.write('		<description>' + self.get_description(way) + '</description>\n')
 			current_curvature_level = 0
 			i = 0
 			for segment in way['segments']:
