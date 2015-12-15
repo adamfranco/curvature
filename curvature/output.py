c5b2d48b07018f5818a1172fc3cf50f4a0aaeea5
Adam Franco <afranco@middlebury.edu>
2015-12-15 02:14:18 -0500

    Learned curvature-output-kml.
    Still need to add colorized kml, tab output, and surface output.

--- curvature/output.py
+++ curvature/output.py
@@ -2,40 +2,11 @@ import sys
 import math
 import string
 from collections import Counter
-from zipfile import ZipFile
-from zipfile import ZIP_DEFLATED
 import os
-
-class Output(object):
-	max_curvature = 0
-
-	def __init__(self, filter):
-		self.filter = filter
-
-	def filter_and_sort(self, ways):
-		# Filter out ways that are too short/long or too straight or too curvy
-		ways = self.filter.filter(ways)
-
-		# Sort the ways based on curvature
-		ways = sorted(ways, key=lambda k: k['curvature'])
-
-		for way in ways:
-			if way['curvature'] > self.max_curvature:
-				self.max_curvature = way['curvature']
-
-		return ways
-
-class TabOutput(Output):
-	def output(self, ways):
-		ways = self.filter_and_sort(ways)
-
-		print "Curvature	Length (mi) Distance (mi)	Id				Name  			County"
-		for way in ways:
-			print '%d	%9.2f	%9.2f	%10s	%25s	%20s' % (way['curvature'], way['length'] / 1609, way['distance'] / 1609, way['id'], way['name'], way['county'])
-
 import codecs
 from xml.sax.saxutils import escape
-class KmlOutput(Output):
+
+class KmlOutput(object):
 	units = 'mi'
 	no_compress = False
 
@@ -78,37 +49,38 @@ class KmlOutput(Output):
 		f.write('</Document>\n')
 		f.write('</kml>\n')
 
-	def _filename_suffix(self):
-		return ''
+	def _record_bbox(self, way):
+		if not hasattr(self, 'min_lat') and 'segments' in way and len(way['segments']):
+			self.min_lat = way['segments'][0]['start'][0]
+			self.max_lat = way['segments'][0]['start'][0]
+			self.min_lon = way['segments'][0]['start'][1]
+			self.max_lon = way['segments'][0]['start'][1]
+		way_max_lat = self.get_way_max_lat(way)
+		if way_max_lat > self.max_lat:
+			self.max_lat = way_max_lat
+		way_min_lat = self.get_way_min_lat(way)
+		if way_min_lat < self.min_lat:
+			self.min_lat = way_min_lat
+		way_max_lon = self.get_way_max_lon(way)
+		if way_max_lon > self.max_lon:
+			self.max_lon = way_max_lon
+		way_min_lon = self.get_way_min_lon(way)
+		if way_min_lon < self.min_lon:
+			self.min_lon = way_min_lon
 
-	def _write_region(self, f, ways):
-		min_lat = ways[0]['segments'][0]['start'][0]
-		max_lat = ways[0]['segments'][0]['start'][0]
-		min_lon = ways[0]['segments'][0]['start'][1]
-		max_lon = ways[0]['segments'][0]['start'][1]
-		for way in ways:
-			way_max_lat = self.get_way_max_lat(way)
-			if way_max_lat > max_lat:
-				max_lat = way_max_lat
-			way_min_lat = self.get_way_min_lat(way)
-			if way_min_lat < min_lat:
-				min_lat = way_min_lat
-			way_max_lon = self.get_way_max_lon(way)
-			if way_max_lon > max_lon:
-				max_lon = way_max_lon
-			way_min_lon = self.get_way_min_lon(way)
-			if way_min_lon < min_lon:
-				min_lon = way_min_lon
+	def _write_region(self, f):
+		if not hasattr(self, 'max_lat'):
+			return
 
 # 		f.write('	<!--\n')
 # 		f.write('	<Region>\n')
 		f.write('		<LatLonBox>\n')
-		f.write('			<north>%.6f</north>\n' % (max_lat))
-		f.write('			<south>%.6f</south>\n' % (min_lat))
+		f.write('			<north>%.6f</north>\n' % (self.max_lat))
+		f.write('			<south>%.6f</south>\n' % (self.min_lat))
 		# Note that this won't work for regions crossing longitude 180, but this
 		# should only affect the Russian asian file
-		f.write('			<east>%.6f</east>\n' % (max_lon))
-		f.write('			<west>%.6f</west>\n' % (min_lon))
+		f.write('			<east>%.6f</east>\n' % (self.max_lon))
+		f.write('			<west>%.6f</west>\n' % (self.min_lon))
 		f.write('		</LatLonBox>\n')
 # 		f.write('	</Region>\n')
 # 		f.write('	-->\n')
@@ -148,38 +120,16 @@ class KmlOutput(Output):
 			if segment['end'][1] < way['min_lon']:
 				way['min_lon'] = segment['end'][1]
 
-	def write (self, ways, path, basename):
-		ways = self.filter_and_sort(ways)
-		ways.reverse()
-
-		kml_path = path + '/' + self.get_filename(basename, 'kml')
-		f = codecs.open(kml_path, 'w', "utf-8")
-
+	def head (self, f):
 		self._write_header(f)
-		if len(ways) > 1:
-			self._write_region(f, ways)
-			self._write_ways(f, ways)
-		else:
-			sys.stderr.write('\nWarning no ways available for output into {}'.format(kml_path))
+
+	def write_way(self, f, way):
+		self._record_bbox(way)
+		self._write_way(f, way)
+
+	def foot (self, f):
+		self._write_region(f)
 		self._write_footer(f)
-		f.close()
-
-		if not self.no_compress:
-			with ZipFile(path + '/' + self.get_filename(basename, 'kmz'), 'w', ZIP_DEFLATED) as zip:
-				zip.write(kml_path, 'doc.kml')
-				zip.close()
-			os.remove(kml_path)
-
-	def get_filename(self, basename, extension):
-		filename = basename + '.c_{0:.0f}'.format(self.filter.min_curvature)
-		if self.filter.max_curvature > 0:
-			filename += '-{0:.0f}'.format(self.filter.max_curvature)
-		if self.filter.min_length != 1 or self.filter.max_length > 0:
-			filename += '.l_{0:.0f}'.format(self.filter.min_length)
-		if self.filter.max_length > 0:
-			filename += '-{0:.0f}'.format(self.filter.max_length)
-		filename += self._filename_suffix() + '.' + extension
-		return filename;
 
 	def get_description(self, way):
 		if self.units == 'km':
@@ -231,11 +181,13 @@ class KmlOutput(Output):
 
 class SingleColorKmlOutput(KmlOutput):
 
-	relative_color = False
+	min_curvature = 0
+	max_curvature = 4000
 
-	def __init__(self, filter, relative_color):
-		super(SingleColorKmlOutput, self).__init__(filter)
-		self.relative_color = relative_color
+	def __init__(self, min_curvature, max_curvature):
+		super(SingleColorKmlOutput, self).__init__()
+		self.min_curvature = min_curvature
+		self.max_curvature = max_curvature
 
 	def get_styles(self):
 		styles = {'lineStyle0':{'color':'F000E010'}} # Straight roads
@@ -250,23 +202,21 @@ class SingleColorKmlOutput(KmlOutput):
 
 		return styles
 
-	def _write_ways(self, f, ways):
-
-		for way in ways:
-			if 'segments' not in way or not len(way['segments']):
+	def _write_way(self, f, way):
+		if 'segments' not in way or not len(way['segments']):
 # 				sys.stderr.write("\nError: way has no segments: {} \n".format(way['name']))
-				continue
-			f.write('	<Placemark>\n')
-			f.write('		<styleUrl>#' + self.line_style(way) + '</styleUrl>\n')
-			f.write('		<name>' + escape(way['name']) + '</name>\n')
-			f.write('		<description><![CDATA[' + self.get_description(way) + ']]></description>\n')
-			f.write('		<LineString>\n')
-			f.write('			<tessellate>1</tessellate>\n')
-			f.write('			<coordinates>')
-			self._write_segments(f, way['segments']);
-			f.write('</coordinates>\n')
-			f.write('		</LineString>\n')
-			f.write('	</Placemark>\n')
+			return
+		f.write('	<Placemark>\n')
+		f.write('		<styleUrl>#' + self.line_style(way) + '</styleUrl>\n')
+		f.write('		<name>' + escape(way['name']) + '</name>\n')
+		f.write('		<description><![CDATA[' + self.get_description(way) + ']]></description>\n')
+		f.write('		<LineString>\n')
+		f.write('			<tessellate>1</tessellate>\n')
+		f.write('			<coordinates>')
+		self._write_segments(f, way['segments']);
+		f.write('</coordinates>\n')
+		f.write('		</LineString>\n')
+		f.write('	</Placemark>\n')
 
 	def _write_segments(self, f, segments):
 		f.write("%.6f,%6f " %(segments[0]['start'][1], segments[0]['start'][0]))
@@ -275,35 +225,7 @@ class SingleColorKmlOutput(KmlOutput):
 
 
 	def level_for_curvature(self, curvature):
-		if self.relative_color:
-			return self.relative_level_for_curvature(curvature)
-		else:
-			return self.absolute_level_for_curvature(curvature)
-
-	def relative_level_for_curvature(self, curvature):
-		if self.filter.min_curvature > 0:
-			offset = self.filter.min_curvature
-		else:
-			offset = 0
-
-		if curvature < offset:
-			return 0
-
-		curvature_pct = (curvature - offset) / (self.max_curvature - offset)
-
-		# Map ratio to a logarithmic scale to give a better differentiation
-		# between lower-curvature ways. 10,000 is max red.
-		# y = 1-1/(10^(x*2))
-		color_pct = 1 - 1/math.pow(10, curvature_pct * 2)
-
-		level = int(round(510 * color_pct)) + 1
-		return level
-
-	def absolute_level_for_curvature(self, curvature):
-		if self.filter.min_curvature > 0:
-			offset = self.filter.min_curvature
-		else:
-			offset = 0
+		offset = self.min_curvature
 
 		if curvature < offset:
 			return 0
@@ -311,7 +233,7 @@ class SingleColorKmlOutput(KmlOutput):
 		# Define a global max rather than just the maximum found in the input.
 		# This will cause the color levels to be the same across inputs for the same
 		# filter minimum.
-		max = 40000
+		max = self.max_curvature
 
 		curvature_pct = min((curvature - offset) / (max - offset), 1)
 
@@ -329,80 +251,51 @@ class SingleColorKmlOutput(KmlOutput):
 	def line_style(self, way):
 		return 'lineStyle{}'.format(self.level_for_curvature(way['curvature']))
 
-class ReducedPointsSingleColorKmlOutput(SingleColorKmlOutput):
-	num_points = 2
-
-	def __init__(self, filter, relative_color, num_points):
-		super(ReducedPointsSingleColorKmlOutput, self).__init__(filter, relative_color)
-		num_points = int(num_points)
-		if num_points > self.num_points:
-			self.num_points = num_points
-
-	def _write_segments(self, f, segments):
-		num_segments = len(segments)
-		interval = math.ceil((num_segments) / (self.num_points - 1))
-
-		# write the first point
-		f.write("%.6f,%6f " %(segments[0]['start'][1], segments[0]['start'][0]))
-
-		i = 0
-		j = 0
-		for segment in segments:
-			i = i + 1
-			j = j + 1
-
-			# Print the last of the interval, plus the last
-			if j == interval or i == num_segments:
-				f.write("%.6f,%6f " %(segment['end'][1], segment['end'][0]))
-				j = 0
 
 class MultiColorKmlOutput(KmlOutput):
-	def _filename_suffix(self):
-		return '.multicolor'
 
-	def _write_ways(self, f, ways):
+	def head(self, f):
+		super(MultiColorKmlOutput, self).head(f)
 		f.write('	<Style id="folderStyle">\n')
 		f.write('		<ListStyle>\n')
 		f.write('			<listItemType>checkHideChildren</listItemType>\n')
 		f.write('		</ListStyle>\n')
 		f.write('	</Style>\n')
 
-		for way in ways:
-			f.write('	<Folder>\n')
-			f.write('		<styleUrl>#folderStyle</styleUrl>\n')
-			f.write('		<name>' + escape(way['name']) + '</name>\n')
-			f.write('		<description><![CDATA[' + self.get_description(way) + ']]></description>\n')
-			current_curvature_level = 0
-			i = 0
-			for segment in way['segments']:
-				if segment['curvature_level'] != current_curvature_level or not i:
-					current_curvature_level = segment['curvature_level']
-					# Close the open LineString
-					if i:
-						f.write('</coordinates>\n')
-						f.write('			</LineString>\n')
-						f.write('		</Placemark>\n')
-					# Start a new linestring for this level
-					f.write('		<Placemark>\n')
-					if 'eliminated' in segment:
-						f.write('			<styleUrl>#elminiated</styleUrl>\n')
-					else:
-						f.write('			<styleUrl>#lineStyle%d</styleUrl>\n' % (current_curvature_level))
-					f.write('			<LineString>\n')
-					f.write('				<tessellate>1</tessellate>\n')
-					f.write('				<coordinates>')
-					f.write("%.6f,%6f " %(segment['start'][1], segment['start'][0]))
-				f.write("%.6f,%6f " %(segment['end'][1], segment['end'][0]))
-				i = i + 1
-			if i:
-				f.write('</coordinates>\n')
-				f.write('			</LineString>\n')
-				f.write('		</Placemark>\n')
-			f.write('	</Folder>\n')
+	def _write_way(self, f, way):
+		f.write('	<Folder>\n')
+		f.write('		<styleUrl>#folderStyle</styleUrl>\n')
+		f.write('		<name>' + escape(way['name']) + '</name>\n')
+		f.write('		<description><![CDATA[' + self.get_description(way) + ']]></description>\n')
+		current_curvature_level = 0
+		i = 0
+		for segment in way['segments']:
+			if segment['curvature_level'] != current_curvature_level or not i:
+				current_curvature_level = segment['curvature_level']
+				# Close the open LineString
+				if i:
+					f.write('</coordinates>\n')
+					f.write('			</LineString>\n')
+					f.write('		</Placemark>\n')
+				# Start a new linestring for this level
+				f.write('		<Placemark>\n')
+				if 'eliminated' in segment:
+					f.write('			<styleUrl>#elminiated</styleUrl>\n')
+				else:
+					f.write('			<styleUrl>#lineStyle%d</styleUrl>\n' % (current_curvature_level))
+				f.write('			<LineString>\n')
+				f.write('				<tessellate>1</tessellate>\n')
+				f.write('				<coordinates>')
+				f.write("%.6f,%6f " %(segment['start'][1], segment['start'][0]))
+			f.write("%.6f,%6f " %(segment['end'][1], segment['end'][0]))
+			i = i + 1
+		if i:
+			f.write('</coordinates>\n')
+			f.write('			</LineString>\n')
+			f.write('		</Placemark>\n')
+		f.write('	</Folder>\n')
 
 class SurfaceKmlOutput(SingleColorKmlOutput):
-	def __init__(self, filter):
-		super(SurfaceKmlOutput, self).__init__(filter, False)
 
 	def get_styles(self):
 		return {
@@ -435,15 +328,6 @@ class SurfaceKmlOutput(SingleColorKmlOutput):
 
 	def line_style(self, way):
 		return way['surface']
-
-	def get_filename(self, basename, extension):
-		filename = basename + '.surfaces'
-		if self.filter.min_length > 0 or self.filter.max_length > 0:
-			filename += '.l_{0:.0f}'.format(self.filter.min_length)
-		if self.filter.max_length > 0:
-			filename += '-{0:.0f}'.format(self.filter.max_length)
-		filename += self._filename_suffix() + '.' + extension
-		return filename;
 
 	def get_description(self, way):
 		return 'Type: %s\nSurface: %s' % (way['type'], way['surface'])
