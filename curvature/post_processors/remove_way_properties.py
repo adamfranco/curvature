529f50d736e423390555f61849e3228e5aabf6ba
Adam Franco <afranco@middlebury.edu>
2016-05-12 22:02:28 -0400

    remove_fields is now remove_way_properties for naming consistency
    #26 Refactor post-processors to use new data structure.

---
+++ curvature/post_processors/remove_way_properties.py
@@ -0,0 +1,24 @@
+# -*- coding: UTF-8 -*-
+import argparse
+
+class RemoveWayProperties(object):
+    def __init__(self, properties):
+        self.properties = properties
+
+    @classmethod
+    def parse(cls, argv):
+        parser = argparse.ArgumentParser(description='Strip certain properties from the ways in the data.')
+        parser.add_argument('--properties', type=str, help='A comma-separated list of the keys to strip. Example --properties refs,coords')
+        args = parser.parse_args(argv)
+        if not args.properties:
+            raise RuntimeError("Error:   --properties must be specified and contain at least 1 property.\n")
+        to_strip = args.properties.split(',')
+        return cls(to_strip)
+
+    def process(self, iterable):
+        for collection in iterable:
+            for way in collection:
+                for property in self.properties:
+                    if property in way:
+                        del way[property]
+            yield(collection)
