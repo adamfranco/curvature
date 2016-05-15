f849bf0f95ba88af72dce7b979f2615993039df2
Fonsan <fonsan@gmail.com>
2016-05-15 22:31:23 +0200

    Made pp_example portable

--- processing_chains/pp_example.sh
+++ processing_chains/pp_example.sh
@@ -1,11 +1,18 @@
-bin/curvature-pp filter_out_ways_with_tag --tag surface --values 'unpaved,dirt,gravel,fine_gravel,sand,grass,ground,pebblestone,mud,clay,dirt/sand,soil' \
-| bin/curvature-pp filter_out_ways_with_tag --tag service --values 'driveway,parking_aisle,drive-through,parking,bus,emergency_access' \
-| bin/curvature-pp add_segments \
-| bin/curvature-pp add_segment_length_and_radius \
-| bin/curvature-pp add_segment_curvature \
-| bin/curvature-pp filter_segment_deflections \
-| bin/curvature-pp split_collections_on_straight_segments --length 2414 \
-| bin/curvature-pp add_way_length \
-| bin/curvature-pp add_way_curvature \
-| bin/curvature-pp filter_collections_by_curvature --min 300 \
-| bin/curvature-pp sort_collections_by_sum --key curvature --direction DESC \
+# Store our the program path.
+pushd `dirname $0` > /dev/null
+my_path=`pwd -P`
+popd > /dev/null
+script_path=`dirname $my_path`
+script_path="${script_path}/bin"
+
+$script_path/curvature-pp filter_out_ways_with_tag --tag surface --values 'unpaved,dirt,gravel,fine_gravel,sand,grass,ground,pebblestone,mud,clay,dirt/sand,soil' \
+| $script_path/curvature-pp filter_out_ways_with_tag --tag service --values 'driveway,parking_aisle,drive-through,parking,bus,emergency_access' \
+| $script_path/curvature-pp add_segments \
+| $script_path/curvature-pp add_segment_length_and_radius \
+| $script_path/curvature-pp add_segment_curvature \
+| $script_path/curvature-pp filter_segment_deflections \
+| $script_path/curvature-pp split_collections_on_straight_segments --length 2414 \
+| $script_path/curvature-pp add_way_length \
+| $script_path/curvature-pp add_way_curvature \
+| $script_path/curvature-pp filter_collections_by_curvature --min 300 \
+| $script_path/curvature-pp sort_collections_by_sum --key curvature --direction DESC
