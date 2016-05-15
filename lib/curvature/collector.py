adamfranco/curvature

commit 259d9db24d3327e43403aacb6910380ffb5e2274

Assume a callback interface for collector Move all verbose output of collector into collector.py

bin/curvature-collect
@@ -45,27 +45,14 @@ collector.verbose = args.v
 if args.highway_types:
     collector.roads = args.highway_types.split(',')
 
+def output(collection):
+    sys.stdout.write(msgpack.packb(collection))
 # start parsing
 for file in args.file:
     if args.v:
         sys.stderr.write("\nLoading {}".format(file.name))
 
-    collector.load_file(file.name)
-    if args.v:
-        total = len(collector.collections)
-        sys.stderr.write("\nStreaming data for {} collections. Each '.' is 1% complete\n".format(total))
-        if total < 100:
-            marker = 1
-        else:
-            marker = round(total/100)
-        i = 0
-        start_time = time.time()
-    for collection in collector.collections:
-        sys.stdout.write(msgpack.packb(collection))
-        if args.v:
-            i += 1
-            if not i % marker:
-                sys.stderr.write('.')
+    collector.parse(file.name, output)
 
 if args.v:
     sys.stderr.write('\nStreaming completed in {time:.1f} seconds. {mem:.1f}MB memory used.\n'.format(time=(time.time() - start_time), mem=resource.getrusage(resource.RUSAGE_SELF).ru_maxrss / 1048576))

lib/curvature/collector.py
@@ -20,12 +20,14 @@ class WayCollector(object):
     def __init__(self, parser_class=OSMParser):
         self.parser_class = parser_class
 
-    def load_file(self, filename):
+    def parse(self, filename, callback):
         # Reinitialize if we have a new file
         collections = []
         coords = {}
         num_coords = 0
         num_ways = 0
+        if self.verbose:
+            sys.stderr.write("\nLoading {}".format(file.name))
 
         # status output
         if self.verbose:
@@ -65,6 +67,23 @@ class WayCollector(object):
         if self.verbose:
             sys.stderr.write("\nJoining complete. {mem:.1f}MB memory used.".format(mem=resource.getrusage(resource.RUSAGE_SELF).ru_maxrss / 1048576))
             sys.stderr.flush()
+
+        if self.verbose:
+            total = len(self.collections)
+            sys.stderr.write("\nStreaming data for {} collections. Each '.' is 1% complete\n".format(total))
+            if total < 100:
+                marker = 1
+            else:
+                marker = round(total/100)
+            i = 0
+            start_time = time.time()
+
+        for collection in self.collections:
+            callback(collection)
+            if self.verbose:
+                i += 1
+                if not i % marker:
+                    sys.stderr.write('.')
 
     def coords_callback(self, coords):
         # callback method for coords

test/post_processors/test_collector.py
@@ -96,12 +96,13 @@ class RoadAParser(MockOSMParser):
 
 def test_collector_road_a():
     collector = WayCollector(parser_class=RoadAParser)
-    collector.load_file('doesnt_exist.pbf')
-    assert len(collector.collections) == 1
-    assert len(collector.collections[0]) == 5
+    collections = []
+    collector.parse('doesnt_exist.pbf', callback=lambda collection: collections.append(collection))
+    assert len(collections) == 1
+    assert len(collections[0]) == 5
 
     # Joining happened in ascending order.
-    if collector.collections[0][0]['id'] == 10004:
+    if collections[0][0]['id'] == 10004:
         expected_asc= [{'coords': [(-73.00009, 43.70009),
                                   (-73.0001, 43.7001),
                                   (-73.00011, 43.70011),
@@ -138,7 +139,7 @@ def test_collector_road_a():
                         'refs': [21, 22, 23, 24],
                         'tags': {'highway': 'unclassified', 'name': 'Road A'}}
                     ]
-        assert collector.collections[0] == expected_asc, "If joined in ascending order, collection must match."
+        assert collections[0] == expected_asc, "If joined in ascending order, collection must match."
     # joining happened in descending order
     else:
         expected_desc= [{'coords': [(-73.00024, 43.70024),
@@ -177,4 +178,4 @@ def test_collector_road_a():
                        'refs': [12, 11, 10, 9],
                        'tags': {'highway': 'unclassified', 'name': 'Road A'}}]
 
-        assert collector.collections[0] == expected_desc, "If joined in descending order, collection must match."
+        assert collections[0] == expected_desc, "If joined in descending order, collection must match."

commit d2d7bb0c5b3bbb49d0218ba1dbf54ae654e4ccb1

Moved a verbose message that was failing and missed in #32

bin/curvature-collect
@@ -53,6 +53,3 @@ for file in args.file:
         sys.stderr.write("\nLoading {}".format(file.name))
 
     collector.parse(file.name, output)
-
-if args.v:
-    sys.stderr.write('\nStreaming completed in {time:.1f} seconds. {mem:.1f}MB memory used.\n'.format(time=(time.time() - start_time), mem=resource.getrusage(resource.RUSAGE_SELF).ru_maxrss / 1048576))

lib/curvature/collector.py
@@ -88,6 +88,8 @@ class WayCollector(object):
                 i += 1
                 if not i % marker:
                     sys.stderr.write('.')
+        if self.verbose:
+            sys.stderr.write('\nStreaming completed in {time:.1f} seconds. {mem:.1f}MB memory used.\n'.format(time=(time.time() - start_time), mem=resource.getrusage(resource.RUSAGE_SELF).ru_maxrss / 1048576))
 
     def coords_callback(self, coords):
         # callback method for coords