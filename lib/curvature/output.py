adamfranco/curvature

commit da97e0a3033cf2dd5527874a8143929c3856270f

Updated the collection data structure to be a dict with a 'ways' key.
This allows the collection to have its own data appended to it independent
of its constituent ways.

lib/curvature/output.py
@@ -56,7 +56,7 @@ class KmlOutput(object):
         f.write('</kml>\n')
 
     def _record_collection_bbox(self, collection):
-        for way in collection:
+        for way in collection['ways']:
             if not hasattr(self, 'min_lat') and 'segments' in way and len(way['segments']):
                 self.min_lat = way['segments'][0]['start'][0]
                 self.max_lat = way['segments'][0]['start'][0]
@@ -113,33 +113,33 @@ class KmlOutput(object):
         return way['min_lon']
 
     def get_collection_max_lat(self, collection):
-        max = self.get_way_max_lat(collection[0])
-        for i in range(1, len(collection)):
-            way_max = self.get_way_max_lat(collection[i])
+        max = self.get_way_max_lat(collection['ways'][0])
+        for i in range(1, len(collection['ways'])):
+            way_max = self.get_way_max_lat(collection['ways'][i])
             if way_max > max:
                 max = way_max
         return max
 
     def get_collection_min_lat(self, collection):
-        min = self.get_way_min_lat(collection[0])
-        for i in range(1, len(collection)):
-            way_min = self.get_way_min_lat(collection[i])
+        min = self.get_way_min_lat(collection['ways'][0])
+        for i in range(1, len(collection['ways'])):
+            way_min = self.get_way_min_lat(collection['ways'][i])
             if way_min < min:
                 min = way_min
         return min
 
     def get_collection_max_lon(self, collection):
-        max = self.get_way_max_lon(collection[0])
-        for i in range(1, len(collection)):
-            way_max = self.get_way_max_lon(collection[i])
+        max = self.get_way_max_lon(collection['ways'][0])
+        for i in range(1, len(collection['ways'])):
+            way_max = self.get_way_max_lon(collection['ways'][i])
             if way_max > max:
                 max = way_max
         return max
 
     def get_collection_min_lon(self, collection):
-        min = self.get_way_min_lon(collection[0])
-        for i in range(1, len(collection)):
-            way_min = self.get_way_min_lon(collection[i])
+        min = self.get_way_min_lon(collection['ways'][0])
+        for i in range(1, len(collection['ways'])):
+            way_min = self.get_way_min_lon(collection['ways'][i])
             if way_min < min:
                 min = way_min
         return min
@@ -172,14 +172,18 @@ class KmlOutput(object):
 
     def get_collection_segments(self, collection):
         segments = []
-        for way in collection:
-            for segment in way['segments']:
-                segments.append(segment)
+        try:
+            for way in collection['ways']:
+                for segment in way['segments']:
+                    segments.append(segment)
+        except TypeError as t:
+            sys.stderr.write('No ways in {}'.format(collection))
+            raise
         return segments
 
     def get_collection_curvature(self, collection):
         total = 0
-        for way in collection:
+        for way in collection['ways']:
             total += self.get_way_curvature(way)
         return total
 
@@ -196,7 +200,7 @@ class KmlOutput(object):
 
     def get_collection_length(self, collection):
         total = 0
-        for way in collection:
+        for way in collection['ways']:
             total += self.get_way_length(way)
         return total
 
@@ -213,7 +217,7 @@ class KmlOutput(object):
 
     def get_length_weighted_collection_tags(self, collection, tag, value_if_empty=None):
         values = {}
-        for way in collection:
+        for way in collection['ways']:
             if tag in way['tags']:
                 value = way['tags'][tag]
             elif value_if_empty:
@@ -229,20 +233,20 @@ class KmlOutput(object):
     def get_shared_collection_refs(self, collection):
         # If the first way doesn't have a ref tag, there is no way there will
         # be one shared by all ways.
-        if not 'ref' in collection[0]['tags']:
+        if not 'ref' in collection['ways'][0]['tags']:
             return set()
 
         # Beginning with the refs on our first way, check each way to find the refs
         # that are common to all.
-        shared_refs = set(collection[0]['tags']['ref'].split(';'))
-        for i in range(1, len(collection)):
+        shared_refs = set(collection['ways'][0]['tags']['ref'].split(';'))
+        for i in range(1, len(collection['ways'])):
             # If the any way doesn't have a ref tag, there is no way there will
             # be one shared by all ways.
-            if not 'ref' in collection[i]['tags']:
+            if not 'ref' in collection['ways'][i]['tags']:
                 return set()
 
             # Reduce our shared_refs set to only those also in the next way.
-            shared_refs = shared_refs & set(collection[i]['tags']['ref'].split(';'))
+            shared_refs = shared_refs & set(collection['ways'][i]['tags']['ref'].split(';'))
             # No need to look further if we have no shared_refs
             if not shared_refs:
                 return shared_refs
@@ -263,7 +267,7 @@ class KmlOutput(object):
         elif names:
             return unicode('{}').format(names[0])
         else:
-            return '{}'.format(collection[0]['id'])
+            return '{}'.format(collection['ways'][0]['id'])
 
     def get_collection_description(self, collection):
         curvature = self.get_collection_curvature(collection)
@@ -291,7 +295,7 @@ class KmlOutput(object):
 
     def get_all_josm_link(self, collection):
         select = []
-        for way in collection:
+        for way in collection['ways']:
             select.append('way%d' % way['id'])
         josm_url ='http://127.0.0.1:8111/load_and_zoom?left=%.5f&right=%.5f&top=%.5f&bottom=%.5f&select=%s' % (self.get_collection_min_lon(collection) - 0.001, self.get_collection_max_lon(collection) + 0.001, self.get_collection_max_lat(collection) + 0.001, self.get_collection_min_lat(collection) - 0.001, ','.join(select))
         josm_link = '<a href="" onclick="var img=document.createElement(\'img\'); img.style.display=\'none\'; img.src=\'%s\'; this.parentElement.appendChild(img); return false;">Edit all in JOSM</a>' % (josm_url)
@@ -303,7 +307,7 @@ class KmlOutput(object):
     def get_constituent_list(self, collection):
         list = '<table style="width: 100%; text-align: center;">'
         list += '<tr><th>View</th><th>Surface</th><th>Actions</th></tr>'
-        for way in collection:
+        for way in collection['ways']:
             view_link = '<a href="https://www.openstreetmap.org/way/%d">%d</a>' % (way['id'], way['id'])
             josm_url = 'http://127.0.0.1:8111/load_and_zoom?left=%.5f&right=%.5f&top=%.5f&bottom=%.5f&select=way%d' % (self.get_way_min_lon(way) - 0.001, self.get_way_max_lon(way) + 0.001, self.get_way_max_lat(way) + 0.001, self.get_way_min_lat(way) - 0.001, way['id'])
             josm_link = '<a href="" onclick="var img=document.createElement(\'img\'); img.style.display=\'none\'; img.src=\'%s\'; this.parentElement.appendChild(img); return false;">Edit in JOSM</a>' % (josm_url)
@@ -479,8 +483,8 @@ class SurfaceKmlOutput(SingleColorKmlOutput):
         return [way]
 
     def get_collection_line_style(self, collection):
-        if 'surface' in collection[0]['tags']:
-            return collection[0]['tags']['surface']
+        if 'surface' in collection['ways'][0]['tags']:
+            return collection['ways'][0]['tags']['surface']
         else:
             return 'unknown'
 
@@ -489,12 +493,12 @@ class SurfaceKmlOutput(SingleColorKmlOutput):
             super(SurfaceKmlOutput, self)._write_collection(f, [way])
 
     def get_collection_description(self, collection):
-        if 'highway' in collection[0]['tags']:
-            highway = collection[0]['tags']['highway']
+        if 'highway' in collection['ways'][0]['tags']:
+            highway = collection['ways'][0]['tags']['highway']
         else:
             highway = ''
-        if 'surface' in collection[0]['tags']:
-            surface = collection[0]['tags']['surface']
+        if 'surface' in collection['ways'][0]['tags']:
+            surface = collection['ways'][0]['tags']['surface']
         else:
             surface = 'unknown'
         description = 'Type: %s\nSurface: %s\n' % (highway, surface)

bin/curvature-pp
@@ -27,5 +27,5 @@ fl = getattr(mod, class_name)
 unpacker = msgpack.Unpacker(sys.stdin, use_list=True)
 post_processor = fl.parse(argv)
 iterable = post_processor.process(unpacker)
-for item in iterable:
-    sys.stdout.write(msgpack.packb(item))
+for collection in iterable:
+    sys.stdout.write(msgpack.packb(collection))

lib/curvature/collection_tools.py
@@ -1,0 +1,7 @@
+
+class CollectionSplitter(object):
+
+    def create_result_collection(self, collection):
+      result_collection = dict((key,value) for key, value in collection.iteritems() if key != 'ways')
+      result_collection['ways'] = []
+      return result_collection

lib/curvature/collector.py
@@ -124,15 +124,19 @@ class WayCollector(object):
                     routes = tags['ref'].split(';')
                     for route in routes:
                         if route not in self.routes:
-                            self.routes[route] = []
-                        self.routes[route].append(way)
+                            self.routes[route] = {  'join_type': 'ref',
+                                                    'join_data': route,
+                                                    'ways': []}
+                        self.routes[route]['ways'].append(way)
                 else:
                     if 'name' in tags and tags['name'] != '':
                         if tags['name'] not in self.routes:
-                            self.routes[tags['name']] = []
-                        self.routes[tags['name']].append(way)
+                            self.routes[tags['name']] = {   'join_type': 'name',
+                                                            'join_data': tags['name'],
+                                                            'ways': []}
+                        self.routes[tags['name']]['ways'].append(way)
                     else:
-                        self.collections.append([way])
+                        self.collections.append({'join_type': 'none', 'ways': [way]})
 
                 for ref in refs:
                     self.coords[ref] = None
@@ -161,8 +165,8 @@ class WayCollector(object):
             sys.stderr.flush()
 
         # Add to joinable-ways.
-        for route, ways in self.routes.iteritems():
-            for way in ways:
+        for route, route_data in self.routes.iteritems():
+            for way in route_data['ways']:
                 way['coords'] = map(lambda ref: self.coords[ref], way['refs'])
             # status output
             if self.verbose:
@@ -173,7 +177,7 @@ class WayCollector(object):
 
         # Add to un-joinable ways.
         for collection in self.collections:
-            for way in collection:
+            for way in collection['ways']:
                 way['coords'] = map(lambda ref: self.coords[ref], way['refs'])
             # status output
             if self.verbose:
@@ -199,7 +203,8 @@ class WayCollector(object):
             sys.stderr.write("\n{} routes will be joined, each '.' is 1% complete\n".format(total))
             sys.stderr.flush()
 
-        for route, ways in self.routes.iteritems():
+        for route, route_data in self.routes.iteritems():
+            ways = route_data['ways']
             # status output
             if self.verbose:
                 i = i + 1
@@ -208,10 +213,12 @@ class WayCollector(object):
                     sys.stderr.flush()
 
             while len(ways) > 0:
-                collection = [ways.pop()]
+                collection = {  'join_type': route_data['join_type'],
+                                'join_data': route_data['join_data'],
+                                'ways': [ways.pop()] }
                 # A list of all refs added to a collection. Checking this list will
                 # prevent creating non-linar forking structures.
-                collection_refs = [collection[0]['refs']]
+                collection_refs = [collection['ways'][0]['refs']]
                 # Loop through all our ways at least as many times as we have ways
                 # to be able to catch any that join onto the end after others have
                 # been joined on.
@@ -231,36 +238,36 @@ class WayCollector(object):
                     while len(ways) > 0:
                         way = ways.pop()
                         # join to the end of the base in order
-                        if collection[-1]['refs'][-1] == way['refs'][0] and way['refs'][-1] not in collection_refs:
+                        if collection['ways'][-1]['refs'][-1] == way['refs'][0] and way['refs'][-1] not in collection_refs:
                             collection_modified = True
-                            collection.append(way)
+                            collection['ways'].append(way)
                             collection_refs = collection_refs + way['refs']
                         # join to the end of the base in reverse order
-                        elif collection[-1]['refs'][-1] == way['refs'][-1] and way['refs'][0] not in collection_refs:
+                        elif collection['ways'][-1]['refs'][-1] == way['refs'][-1] and way['refs'][0] not in collection_refs:
                             # Make a copy of the way before modifying it as it may be
                             # a member of other routes that will be joined in a different sequence.
                             way_copy = copy(way)
                             way_copy['refs'] = list(reversed(way_copy['refs']))
                             way_copy['coords'] = list(reversed(way_copy['coords']))
                             collection_modified = True
-                            collection.append(way_copy)
+                            collection['ways'].append(way_copy)
                             collection_refs = collection_refs + way_copy['refs']
 
                         # join to the beginning of the base in order
-                        elif collection[0]['refs'][0] == way['refs'][-1] and way['refs'][0] not in collection_refs:
+                        elif collection['ways'][0]['refs'][0] == way['refs'][-1] and way['refs'][0] not in collection_refs:
                             collection_modified = True
-                            collection.insert(0, way)
+                            collection['ways'].insert(0, way)
                             collection_refs =  way['refs'] + collection_refs
 
                         # join to the beginning of the base in reverse order
-                        elif collection[0]['refs'][0] == way['refs'][0] and way['refs'][-1] not in collection_refs:
+                        elif collection['ways'][0]['refs'][0] == way['refs'][0] and way['refs'][-1] not in collection_refs:
                             # Make a copy of the way before modifying it as it may be
                             # a member of other routes that will be joined in a different sequence.
                             way_copy = copy(way)
                             collection_modified = True
                             way_copy['refs'] = list(reversed(way_copy['refs']))
                             way_copy['coords'] = list(reversed(way_copy['coords']))
-                            collection.insert(0, way_copy)
+                            collection['ways'].insert(0, way_copy)
                             collection_refs = way_copy['refs'] + collection_refs
                         else:
                             unused_ways.append(way)

lib/curvature/post_processors/add_segment_curvature.py
@@ -18,7 +18,7 @@ class AddSegmentCurvature(object):
     def process(self, iterable):
         for collection in iterable:
             all_segments = []
-            for way in collection:
+            for way in collection['ways']:
                 if 'segments' not in way:
                     raise ValueError('Required "segments" not found in way. Add them with `curvature-pp add_segments` before using this processor.')
                 for segment in way['segments']:

lib/curvature/post_processors/add_segment_length_and_radius.py
@@ -11,7 +11,7 @@ class AddSegmentLengthAndRadius(object):
     def process(self, iterable):
         for collection in iterable:
             all_segments = []
-            for way in collection:
+            for way in collection['ways']:
                 if 'segments' not in way:
                     raise ValueError('Required "segments" not found in way. Add them with `curvature-pp add_segments` before using this processor.')
                 all_segments = all_segments + way['segments']

lib/curvature/post_processors/add_segments.py
@@ -8,7 +8,7 @@ class AddSegments(object):
 
     def process(self, iterable):
         for collection in iterable:
-            for way in collection:
+            for way in collection['ways']:
                 way['segments'] = []
                 i = 0
                 for coord in way['coords']:

lib/curvature/post_processors/add_way_curvature.py
@@ -6,7 +6,7 @@ class AddWayCurvature(object):
 
     def process(self, iterable):
         for collection in iterable:
-            for way in collection:
+            for way in collection['ways']:
                 sum = 0
                 for segment in way['segments']:
                     sum += segment['curvature']

commit 7c1c77092212d3baa61768c4a608166a1d4a0bad

Fixed Surface output broken in da97e0a30 and removed extra debugging added there.

lib/curvature/output.py
@@ -172,13 +172,9 @@ class KmlOutput(object):
 
     def get_collection_segments(self, collection):
         segments = []
-        try:
-            for way in collection['ways']:
-                for segment in way['segments']:
-                    segments.append(segment)
-        except TypeError as t:
-            sys.stderr.write('No ways in {}'.format(collection))
-            raise
+        for way in collection['ways']:
+            for segment in way['segments']:
+                segments.append(segment)
         return segments
 
     def get_collection_curvature(self, collection):
@@ -489,8 +485,8 @@ class SurfaceKmlOutput(SingleColorKmlOutput):
             return 'unknown'
 
     def _write_collection(self, f, collection):
-        for way in collection:
-            super(SurfaceKmlOutput, self)._write_collection(f, [way])
+        for way in collection['ways']:
+            super(SurfaceKmlOutput, self)._write_collection(f, {'join_type': 'none', 'ways': [way]})
 
     def get_collection_description(self, collection):
         if 'highway' in collection['ways'][0]['tags']: