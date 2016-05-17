9a5b5c8768a989b1707d8ea7c2b20072a2b5cd54
Adam Franco <afranco@middlebury.edu>
2016-05-16 23:17:10 -0400

    Test joining by 'ref' in addition to 'name'

--- test/post_processors/test_collector.py
+++ test/post_processors/test_collector.py
@@ -98,6 +98,69 @@ class RoadAParser(MockOSMParser):
         (24, 43.70024, -73.00024)
     ]
 
+class Vermont125Parser(MockOSMParser):
+
+    ways = [
+        (
+            20000,
+            {   'name':     'South Main Street',
+                'ref':      'VT 125',
+                'highway':  'secondary'},
+            [215, 216, 217, 218]
+        ),
+        # Can be appended in normal order
+        (
+            20001,
+            {   'name':     'Court Street',
+                'ref':      'US 7;VT 125',
+                'highway':  'primary'},
+            [218, 219, 220, 221]
+        ),
+        # Can be appended in reverse order
+        (
+            20002,
+            {   'name':     'Main Street',
+                'ref':      'VT 125;VT 30',
+                'highway':  'secondary'},
+            [224, 223, 222, 221]
+        ),
+        # Can be pre-pended in normal order
+        (
+            20003,
+            {   'name':     'Ripton Road',
+                'ref':      'VT 125',
+                'highway':  'secondary'},
+            [212, 213, 214, 215]
+        ),
+        # Can be pre-pended in reverse order
+        (
+            20004,
+            {   'name':     'Hancock Road',
+                'ref':      'VT 125',
+                'highway':  'secondary'},
+            [212, 211, 210, 209]
+        ),
+    ]
+
+    coords = [
+        (209, 43.70009, -73.00009),
+        (210, 43.70010, -73.00010),
+        (211, 43.70011, -73.00011),
+        (212, 43.70012, -73.00012),
+        (213, 43.70013, -73.00013),
+        (214, 43.70014, -73.00014),
+        (215, 43.70015, -73.00015),
+        (216, 43.70016, -73.00016),
+        (217, 43.70017, -73.00017),
+        (218, 43.70018, -73.00018),
+        (219, 43.70019, -73.00019),
+        (220, 43.70020, -73.00020),
+        (221, 43.70021, -73.00021),
+        (222, 43.70022, -73.00022),
+        (223, 43.70023, -73.00023),
+        (224, 43.70024, -73.00024)
+    ]
+
 def test_collector_road_a():
     collector = WayCollector(parser_class=RoadAParser)
     collections = []
@@ -149,6 +212,65 @@ def test_collector_road_a():
     # joining happened in descending order
     else:
         assert collections[0]['ways'] == reverse(expected_asc), "If joined in descending order, collection must match."
+
+def test_collector_ref_matching():
+    collector = WayCollector(parser_class=Vermont125Parser)
+    collections = []
+
+    collector.parse('doesnt_exist.pbf', callback=lambda collection: collections.append(collection))
+    assert len(collections) == 3 # VT 30, VT 125, US 7
+    # Get the collection for VT 125.
+    found = False
+    for collection in collections:
+        if collection['join_type'] == 'ref' and collection['join_data'] == 'VT 125':
+            found = True
+            break
+    assert len(collection['ways']) == 5
+    assert collection['join_type'] == 'ref'
+    assert collection['join_data'] == 'VT 125'
+
+    expected_asc= [{'coords': [(-73.00009, 43.70009),
+                          (-73.0001, 43.7001),
+                          (-73.00011, 43.70011),
+                          (-73.00012, 43.70012)],
+               'id': 20004,
+               'refs': [209, 210, 211, 212],
+               'tags': {'highway': 'secondary', 'name': 'Hancock Road', 'ref': 'VT 125'}},
+              {'coords': [(-73.00012, 43.70012),
+                          (-73.00013, 43.70013),
+                          (-73.00014, 43.70014),
+                          (-73.00015, 43.70015)],
+               'id': 20003,
+               'refs': [212, 213, 214, 215],
+               'tags': {'highway': 'secondary', 'name': 'Ripton Road', 'ref': 'VT 125'}},
+              {'coords': [(-73.00015, 43.70015),
+                          (-73.00016, 43.70016),
+                          (-73.00017, 43.70017),
+                          (-73.00018, 43.70018)],
+               'id': 20000,
+               'refs': [215, 216, 217, 218],
+               'tags': {'highway': 'secondary', 'name': 'South Main Street', 'ref': 'VT 125'}},
+              {'coords': [(-73.00018, 43.70018),
+                          (-73.00019, 43.70019),
+                          (-73.0002, 43.7002),
+                          (-73.00021, 43.70021)],
+               'id': 20001,
+               'refs': [218, 219, 220, 221],
+               'tags': {'highway': 'primary', 'name': 'Court Street', 'ref': 'US 7;VT 125'}},
+              {'coords': [(-73.00021, 43.70021),
+                          (-73.00022, 43.70022),
+                          (-73.00023, 43.70023),
+                          (-73.00024, 43.70024)],
+                'id': 20002,
+                'refs': [221, 222, 223, 224],
+                'tags': {'highway': 'secondary', 'name': 'Main Street', 'ref': 'VT 125;VT 30'}}
+        ]
+    # Joining happened in ascending order.
+    if collection['ways'][0]['id'] == expected_asc[0]['id']:
+        assert collection['ways'] == expected_asc, "If joined in ascending order, collection must match."
+    # joining happened in descending order
+    else:
+        assert collection['ways'] == reverse(expected_asc), "If joined in descending order, collection must match."
 
 # Reverse an array of ways.
 # This will allow tests to remain valid even if the logic changes for join order,
