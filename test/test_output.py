e96bfcdbe661135d17bf74b16b5d42a316b12b86
Adam Franco <afranco@middlebury.edu>
2016-05-19 20:34:18 -0400

    Reorganized test_collector and test_output locations to match module hierarchy.

--- test/test_output.py
+++ test/test_output.py
@@ -1,6 +1,6 @@
 # Add our parent folder to our path
 import sys, os
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
+sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
 import pytest
 from curvature.output import KmlOutput
 from copy import copy
