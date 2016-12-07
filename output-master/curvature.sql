6dd0589c7687176a391c14f63e239c5e221086b1
Adam Franco <afranco@middlebury.edu>
2016-12-06 23:32:39 -0500

    Now using a lookup table for sources to save on data usage.

--- output-master/curvature.sql
+++ output-master/curvature.sql
@@ -5,7 +5,7 @@
 -- Dumped from database version 9.5.5
 -- Dumped by pg_dump version 9.5.1
 
--- Started on 2016-12-06 18:57:39 EST
+-- Started on 2016-12-06 23:31:35 EST
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -34,7 +34,7 @@ CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
 
 
 --
--- TOC entry 3874 (class 0 OID 0)
+-- TOC entry 3885 (class 0 OID 0)
 -- Dependencies: 1
 -- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
 --
@@ -51,7 +51,7 @@ CREATE EXTENSION IF NOT EXISTS postgis WITH SCHEMA public;
 
 
 --
--- TOC entry 3875 (class 0 OID 0)
+-- TOC entry 3886 (class 0 OID 0)
 -- Dependencies: 2
 -- Name: EXTENSION postgis; Type: COMMENT; Schema: -; Owner: 
 --
@@ -68,7 +68,7 @@ CREATE EXTENSION IF NOT EXISTS postgis_topology WITH SCHEMA topology;
 
 
 --
--- TOC entry 3876 (class 0 OID 0)
+-- TOC entry 3887 (class 0 OID 0)
 -- Dependencies: 3
 -- Name: EXTENSION postgis_topology; Type: COMMENT; Schema: -; Owner: 
 --
@@ -88,19 +88,73 @@ SET default_with_oids = false;
 --
 
 CREATE TABLE curvature_segments (
-    id character varying(100) NOT NULL,
-    source character varying(50),
+    id character(41) NOT NULL,
     name character varying(50),
     curvature integer,
     geom geometry(LineString),
-    paved boolean DEFAULT false NOT NULL
+    paved boolean DEFAULT false NOT NULL,
+    fk_source integer
 );
 
 
 ALTER TABLE curvature_segments OWNER TO postgres;
 
 --
--- TOC entry 3745 (class 2606 OID 19496)
+-- TOC entry 3888 (class 0 OID 0)
+-- Dependencies: 205
+-- Name: COLUMN curvature_segments.id; Type: COMMENT; Schema: public; Owner: postgres
+--
+
+COMMENT ON COLUMN curvature_segments.id IS 'Sha1 hash of the constituent way-ids.';
+
+
+--
+-- TOC entry 207 (class 1259 OID 28646)
+-- Name: sources; Type: TABLE; Schema: public; Owner: curvature_app
+--
+
+CREATE TABLE sources (
+    id integer NOT NULL,
+    source character varying(100)
+);
+
+
+ALTER TABLE sources OWNER TO curvature_app;
+
+--
+-- TOC entry 206 (class 1259 OID 28644)
+-- Name: sources_id_seq; Type: SEQUENCE; Schema: public; Owner: curvature_app
+--
+
+CREATE SEQUENCE sources_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE sources_id_seq OWNER TO curvature_app;
+
+--
+-- TOC entry 3890 (class 0 OID 0)
+-- Dependencies: 206
+-- Name: sources_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: curvature_app
+--
+
+ALTER SEQUENCE sources_id_seq OWNED BY sources.id;
+
+
+--
+-- TOC entry 3749 (class 2604 OID 28649)
+-- Name: id; Type: DEFAULT; Schema: public; Owner: curvature_app
+--
+
+ALTER TABLE ONLY sources ALTER COLUMN id SET DEFAULT nextval('sources_id_seq'::regclass);
+
+
+--
+-- TOC entry 3752 (class 2606 OID 27410)
 -- Name: curvature_segments_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -109,7 +163,16 @@ ALTER TABLE ONLY curvature_segments
 
 
 --
--- TOC entry 3743 (class 1259 OID 19514)
+-- TOC entry 3755 (class 2606 OID 28651)
+-- Name: sources_pkey; Type: CONSTRAINT; Schema: public; Owner: curvature_app
+--
+
+ALTER TABLE ONLY sources
+    ADD CONSTRAINT sources_pkey PRIMARY KEY (id);
+
+
+--
+-- TOC entry 3750 (class 1259 OID 19514)
 -- Name: curvature_segment_geom; Type: INDEX; Schema: public; Owner: postgres
 --
 
@@ -117,7 +180,24 @@ CREATE INDEX curvature_segment_geom ON curvature_segments USING gist (geom);
 
 
 --
--- TOC entry 3873 (class 0 OID 0)
+-- TOC entry 3753 (class 1259 OID 28657)
+-- Name: fki_foreign_key_source; Type: INDEX; Schema: public; Owner: postgres
+--
+
+CREATE INDEX fki_foreign_key_source ON curvature_segments USING btree (fk_source);
+
+
+--
+-- TOC entry 3756 (class 2606 OID 28652)
+-- Name: foreign_key_source; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY curvature_segments
+    ADD CONSTRAINT foreign_key_source FOREIGN KEY (fk_source) REFERENCES sources(id) ON UPDATE CASCADE ON DELETE CASCADE;
+
+
+--
+-- TOC entry 3884 (class 0 OID 0)
 -- Dependencies: 8
 -- Name: public; Type: ACL; Schema: -; Owner: postgres
 --
@@ -128,7 +208,19 @@ GRANT ALL ON SCHEMA public TO postgres;
 GRANT ALL ON SCHEMA public TO PUBLIC;
 
 
--- Completed on 2016-12-06 18:57:39 EST
+--
+-- TOC entry 3889 (class 0 OID 0)
+-- Dependencies: 205
+-- Name: curvature_segments; Type: ACL; Schema: public; Owner: postgres
+--
+
+REVOKE ALL ON TABLE curvature_segments FROM PUBLIC;
+REVOKE ALL ON TABLE curvature_segments FROM postgres;
+GRANT ALL ON TABLE curvature_segments TO postgres;
+GRANT ALL ON TABLE curvature_segments TO curvature_app;
+
+
+-- Completed on 2016-12-06 23:31:35 EST
 
 --
 -- PostgreSQL database dump complete
