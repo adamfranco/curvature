50135c39183196b3b5dd17fc606b14b462803d92
Adam Franco <afranco@middlebury.edu>
2020-10-11 13:09:14 -0400

    Add library for recording affected areas. #57

---
+++ curvature/tile_tools.py
@@ -0,0 +1,80 @@
+import sys
+import mercantile
+
+# Class for building up an affected area from modified bounding-boxes.
+class AffectedArea(object):
+
+    def __init__(self):
+        self.bbox = None
+
+    # Record the tiles affected by changes in a bounding box.
+    #
+    # bbox is decimal degrees with longitude -180 to +180 and Latitude -90 to +90.
+    def record_affected(self, west, south, east, north):
+        bbox = BBox(west, south, east, north)
+        if self.bbox:
+            self.bbox = self.bbox.union(bbox)
+        else:
+            self.bbox = bbox
+
+    # Answer the affected bounding box
+    def get_affected_bbox(self):
+        return self.bbox
+
+    # Answer the affected tiles at the zoom levels we're concerned with.
+    #
+    # Structure:
+    #   [
+    #     Tile(z=z0, x=x0, y=y0),
+    #     Tile(z=z0, x=x1, y=y1),
+    #     Tile(z=z0, x=x2, y=y2),
+    #     Tile(z=z1, x=x3, y=y3),
+    #     Tile(z=z1, x=x4, y=y4),
+    #     ...
+    #   ]
+    def get_affected_tiles(self, max_zoom=20, min_zoom=None):
+        # Add the tiles at the max zoom.
+        affected_at_max = set(mercantile.tiles(
+            self.bbox.west,
+            self.bbox.south,
+            self.bbox.east,
+            self.bbox.north,
+            [max_zoom],
+            True))
+        affected = affected_at_max
+
+        # Recursively roll up affected tiles to lower zooms.
+        if min_zoom is not None:
+            z = max_zoom
+            tiles = affected_at_max
+            while z > min_zoom:
+                tiles = self.roll_up_affected_tiles(tiles)
+                z = z - 1
+                affected = affected.union(tiles)
+
+        return affected
+
+    def roll_up_affected_tiles(self, tiles):
+        affected = set()
+        for tile in tiles:
+            affected.add(mercantile.parent(tile))
+        return affected
+
+# BBox is decimal degrees with longitude -180 to +180 and Latitude -90 to +90.
+class BBox(object):
+
+    def __init__(self, west, south, east, north):
+        # The minimum zoom-level to calculate affected tiles for.
+        self.west = west
+        self.south = south
+        self.east = east
+        self.north = north
+
+    # Return the union of two BBoxes.
+    def union(self, bbox):
+        unionNorth = max(self.north, bbox.north)
+        unionSouth = min(self.south, bbox.south)
+        # To-do: handle spanning +/- 180.
+        unionWest = min(self.west, bbox.west)
+        unionEast = max(self.east, bbox.east)
+        return BBox(unionWest, unionSouth, unionEast, unionNorth)
---
+++ test/test_affected_tiles.py
@@ -0,0 +1,114 @@
+# Add our parent folder to our path
+import sys, os
+sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
+from curvature.tile_tools import AffectedArea
+from mercantile import Tile
+import time
+
+# Low-zoom tiles.
+def tiles_at_zoom(tiles, zoom=0):
+    tiles = list(filter(lambda t: t.z == zoom, tiles))
+    return sorted(sorted(tiles, key=lambda tile: tile.y), key=lambda tile: tile.x)
+
+def test_single_bbox():
+    a = AffectedArea()
+    a.record_affected(-73.2548, 43.9493, -73.0, 44.0461)
+
+    b = a.get_affected_bbox()
+    assert b.west == -73.2548, "West should be equal"
+    assert b.south == 43.9493, "South should be equal"
+    assert b.east == -73.0, "East should be equal"
+    assert b.north == 44.0461, "North should be equal"
+
+    tiles = a.get_affected_tiles(max_zoom=9, min_zoom=3)
+    expected_tiles = {
+        Tile(x=151, y=186, z=9),
+        Tile(x=152, y=186, z=9),
+        Tile(x=75, y=93, z=8),
+        Tile(x=76, y=93, z=8),
+        Tile(x=37, y=46, z=7),
+        Tile(x=38, y=46, z=7),
+        Tile(x=18, y=23, z=6),
+        Tile(x=19, y=23, z=6),
+        Tile(x=9, y=11, z=5),
+        Tile(x=4, y=5, z=4),
+        Tile(x=2, y=2, z=3)
+    }
+    assert tiles == expected_tiles
+
+def test_overlapping_bboxes():
+    a = AffectedArea()
+    a.record_affected(-73.2548, 43.9493, -73.0, 44.0461)
+    # Add an overlapping region.
+    a.record_affected(-73.5, 44, -73.05, 44.7)
+
+    b = a.get_affected_bbox()
+    assert b.west == -73.5
+    assert b.south == 43.9493
+    assert b.east == -73.0
+    assert b.north == 44.7
+
+    tiles = a.get_affected_tiles(max_zoom=9, min_zoom=3)
+    expected_tiles = {
+        Tile(x=151, y=184, z=9),
+        Tile(x=151, y=185, z=9),
+        Tile(x=151, y=186, z=9),
+        Tile(x=152, y=184, z=9),
+        Tile(x=152, y=185, z=9),
+        Tile(x=152, y=186, z=9),
+        Tile(x=75, y=92, z=8),
+        Tile(x=76, y=92, z=8),
+        Tile(x=75, y=93, z=8),
+        Tile(x=76, y=93, z=8),
+        Tile(x=37, y=46, z=7),
+        Tile(x=38, y=46, z=7),
+        Tile(x=18, y=23, z=6),
+        Tile(x=19, y=23, z=6),
+        Tile(x=9, y=11, z=5),
+        Tile(x=4, y=5, z=4),
+        Tile(x=2, y=2, z=3)
+    }
+    assert tiles == expected_tiles
+
+def test_many_bboxes_for_performance():
+    a = AffectedArea()
+
+    start_time = time.time()
+    # Increment over 100,000 bboxes across 12°
+    start_x = x = -112.44
+    start_y = y = 31.93
+    iterations = 100000
+    deg = 12.0
+    inc_deg = deg / iterations
+    box_size = 0.3
+    a.record_affected(x, y, x + box_size, y + box_size)
+    for i in range(1, iterations + 1):
+        x = x + inc_deg
+        y = y + inc_deg
+        a.record_affected(x, y, x + box_size, y + box_size)
+
+    # Make sure our test math is right.
+    end_x = start_x + deg
+    assert round(x, 4) == round(end_x, 4)
+    end_y = start_y + deg
+    assert round(y, 4) == round(end_y, 4)
+    box_east = end_x + box_size
+    box_north = end_y + box_size
+
+    b = a.get_affected_bbox()
+    elapsed = time.time() - start_time
+    assert elapsed < 1.0, "Recording and reporting on the affected bbox should take less than 1 second"
+    assert b.west == start_x
+    assert b.south == start_y
+    assert round(b.east, 4) == round(box_east, 4)
+    assert round(b.north, 4) == round(box_north, 4)
+
+    tiles = a.get_affected_tiles(max_zoom=6, min_zoom=2)
+    elapsed = time.time() - start_time
+    assert elapsed < 1.0, "Reporting on affected tiles should take less than 1 seconds"
+
+    assert len(tiles_at_zoom(tiles, 2)) == 1
+    assert len(tiles_at_zoom(tiles, 3)) == 2
+    assert len(tiles_at_zoom(tiles, 4)) == 2
+    assert len(tiles_at_zoom(tiles, 5)) == 6
+    assert len(tiles_at_zoom(tiles, 6)) == 12
