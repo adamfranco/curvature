dc2ad23398f68afc992dc007cfcb986f5a1f4cb0
removedporn <86824510+removedporn@users.noreply.github.com>
2021-11-16 22:45:00 +0800

    Delete test directory

--- test/test_radiusmath.py
+++
@@ -1,7 +0,0 @@
-from curvature.radiusmath import circum_circle_radius 
-
-def test_circum_circle_radius_zero_lengths():
-  assert circum_circle_radius(0, 0, 0) == 10000
-
-def test_circum_circle_radius_zero_division():
-  assert circum_circle_radius(0.13430093277996386, 0.13430093277996386, 0.2686018655599277) == 10000
